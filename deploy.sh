f1fa676bf1300298436f5a8ad46928a00ad59477
Stefan Cocora <stefan.cocora@gmail.com>
2016-07-12 15:58:42 +0100

    capturing deployment steps in a poor man's equivalent of a Jenkinsfile

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+[ -n "${DEBUG}" ] && set -x
+
+GIT_COMMIT=${GIT_COMMIT:-$(git rev-parse --short HEAD)}
+GIT_COMMIT=${GIT_COMMIT:0:7}
+
+echo ${GIT_COMMIT} > GIT_COMMIT
+echo "deploy triggered by commit : ${GIT_COMMIT} on repo: ${GIT_URL} on branch ${GIT_BRANCH}"
+echo "I will deploy version: $VERSION"
+
+# running kb8or
+../scripts/deploy -e dev ../pt-income.yaml
